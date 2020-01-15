9890128a33ab50e22ba88f193c57fe3b51929eb2
BuildTools <Rubenbramasco@gmail.com>
2020-01-14 22:51:53 -0800

    Removed two helper functions(gui,item)

--- src/me/rubenbramasco/ChestCreator.java
+++ src/me/rubenbramasco/ChestCreator.java
@@ -1,18 +1,13 @@
 package me.rubenbramasco;
 
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.metadata.MetadataValue;
 
 import net.md_5.bungee.api.ChatColor;
 
-import java.util.ArrayList;
 
 import org.bukkit.Material;
-import org.bukkit.block.Barrel;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -22,6 +17,11 @@ import org.bukkit.event.block.Action;
 public class ChestCreator implements Listener{
 	
 	Player player;
+	TrashCanMC plugin;
+	
+	public ChestCreator(TrashCanMC plugin) {
+		this.plugin = plugin;
+	}
 	
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
@@ -41,35 +41,13 @@ public class ChestCreator implements Listener{
 			
 			if(item.equals(bStick) && 
 					clickedBlock.getBlockData().getMaterial() == Material.BARREL) {
-						player.sendMessage("Trash Can created_test");
-						setGui(clickedBlock);
+						player.sendMessage("Trash Can Created");
+						plugin.setGui(clickedBlock);
 			}
 			
 			
 		}
 		
 	}
-	
-	private void setGui(Block chest) {
-	
-		Barrel trashCanGui = (Barrel)chest.getState();
-		trashCanGui.setCustomName(ChatColor.BOLD + " "+ChatColor.RED + "Trash Can");
-		
-		ItemStack fireButton = new ItemStack(Material.CAMPFIRE);
-		ItemMeta fireButton_Meta = fireButton.getItemMeta();
-		
-		ArrayList<String> lore = new ArrayList<String>();
-		lore.add("Dude if you cick this everything is wiped");
-		
-		fireButton_Meta.setDisplayName("Destory Items");
-		fireButton_Meta.setLore(lore);
-		fireButton_Meta.setLocalizedName("");
-		
-		fireButton.setItemMeta(fireButton_Meta);
-		trashCanGui.getInventory().setItem(26,fireButton); //26 is the last index of the barrel 
-
-		
-	}
-	
 
 }
